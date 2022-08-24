b5f599fcf38dd5d6611564611ad5aaf5ed7e19d4
unknown <popovici.alex96@gmail.com>
2022-08-24 22:15:52 +0300

    fixing shell

--- dbextract.sh
+++ dbextract.sh
@@ -1,8 +1,8 @@
 DIR="~/../var/db/mysql"
 
-if [-d $DIR]
+if [-d $DIR]; then 
     cd ~/../var/db/
     rmdir mysql
-    
+fi  
 cd ~/../usr/metin2server-project
 tar -xd metin2_mysql.tar.gz ~/../var/db/
