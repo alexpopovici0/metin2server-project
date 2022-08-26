f8f4f43d714602c79f435f50bdde7a1215afd0a7
unknown <popovici.alex96@gmail.com>
2022-08-26 21:33:00 +0300

    added convertor for account password

--- webapp/pages/api/hello.js
+++ webapp/pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import crypto from 'crypto'
 
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
+}
+
+export const getSha1=()=>{
+  let hash1=crypto.createHash('sha1').update('123456789').digest();
+  let hash2=crypto.createHash('sha1').update(hash1).digest('hex');
+  return hash2;
 }
